464bbea80cb811c151c539b03db63a5fdb18699f
martint201 <martint.leclercq@gmail.com>
2024-03-22 16:02:47 +0100

    unfinished data analysis (probably not running)

--- src/data_pure.py
+++ src/data_pure.py
@@ -10,55 +10,128 @@ import zipfile
 import json
 from datetime import datetime
 
-#1  path data
-DATA_PATH = "../ESG/"
-print(os.listdir(DATA_PATH))
 
-#2 import data
-esg_light = "EUESGMANUFACTURER-LIGHT.zip"
-esg_light_data = pd.DataFrame()
+def get_clean_data_esg(esg_data):
+    # # 1  path data
+    # DATA_PATH = "../ESG/"
+    # print(os.listdir(DATA_PATH))
 
-with zipfile.ZipFile(DATA_PATH + esg_light, 'r') as z:
-    for filename in z.namelist():
-        if filename.endswith('.csv'):
-            with z.open(filename) as f:
-                esg_light_data = pd.read_csv(f)
+    # # 2 import data
+    # esg_light = "EUESGMANUFACTURER-LIGHT.zip"
+    # esg_light_data = pd.DataFrame()
 
+    # with zipfile.ZipFile(DATA_PATH + esg_light, "r") as z:
+    #     for filename in z.namelist():
+    #         if filename.endswith(".csv"):
+    #             with z.open(filename) as f:
+    #                 esg_light_data = pd.read_csv(f)
 
-#3 Data cleaning, drop unecessary columns 
-columns_to_drop = ["swissValorNumber", "ISIN_BC", "FISN", 
-                   "fundManagerSIXCompanyKey", "fundManagerLEI", "fundManagerLongName",
-                   "companyDomicileISO", "issuerSIXCompanyKey", "LEI",
-                   "ESGFactorDate", "ESGDeliveryDate", "ESGFactor"
-                   ]
-df = esg_light_data.drop(columns=columns_to_drop)
+    # 3 Data cleaning, drop unecessary columns
+    columns_to_drop = [
+        "swissValorNumber",
+        "ISIN_BC",
+        "FISN",
+        "fundManagerSIXCompanyKey",
+        "fundManagerLEI",
+        "fundManagerLongName",
+        "companyDomicileISO",
+        "issuerSIXCompanyKey",
+        "LEI",
+        "ESGFactorDate",
+        "ESGDeliveryDate",
+        "ESGFactor",
+    ]
+    df = esg_data.drop(columns=columns_to_drop)
 
+    # 4  Obtain first version of pure data set
 
-#4  Obtain first version of pure data set
+    # Number of financial products
+    number_financial_products = len(esg_data["ISIN"].unique())
 
-# Number of financial products
-number_financial_products = len(esg_light_data["ISIN"].unique())
+    # Create ProductType, classification of financial products with their ESG Activity
+    X = np.zeros(shape=(number_financial_products, 1))
 
-# Create ProductType, classification of financial products with their ESG Activity
-X = np.zeros(shape=(number_financial_products, 1))
+    # Create separate list to optimize the search
+    article_20040 = df[df["ESGFactorProviderId"] == "20040"]
+    article_20050 = df[df["ESGFactorProviderId"] == "20050"]
 
-# Create separate list to optimize the search
-article_20040 = df[ df['ESGFactorProviderId'] == "20040"]
-article_20050 = df[ df['ESGFactorProviderId'] == "20050"]
+    for i, isin in enumerate(df["ISIN"].unique()):
 
-for i, isin in enumerate(df["ISIN"].unique()):
+        X[i] = article_20040.loc[
+            article_20040["ISIN"] == isin, "ESGFactorAmountLastYear"
+        ].iloc[0]
 
-    X[i] = article_20040.loc[article_20040["ISIN"]== isin, "ESGFactorAmountLastYear"].iloc[0]
+        # If the value is 0, it might be a product under miFID
+        if X[i] == 0:
+            X[i] = article_20050.loc[
+                article_20050["ISIN"] == isin, "ESGFactorAmountLastYear"
+            ].iloc[0]
 
-    # If the value is 0, it might be a product under miFID
-    if X[i] == 0:
-        X[i] = article_20050.loc[article_20050["ISIN"]== isin, "ESGFactorAmountLastYear"].iloc[0]
+    # Flatten X
+    X_flat = X.flatten()
 
-# Flatten X 
-X_flat = X.flatten()
+    # Create a panda frame with the foundamental data
+    df_pure = pd.DataFrame({"ISIN": df["ISIN"].unique(), "ProductType": X_flat})
 
-# Create a panda frame with the foundamental data
-df_pure = pd.DataFrame({
-    "ISIN": df["ISIN"].unique(),
-    "ProductType": X_flat
-})
+    # all classification
+    classification = df["ESGClassification"].unique()[-12:]
+    number_classification = len(classification)
+
+    # Define ESG factors
+    environment = [
+        "Greenhouse gas emissions",
+        "Biodiversity",
+        "Water",
+        "Waste",
+        "Environmental",
+        "Fossil fuels",
+        "Energy efficiency",
+        "Energy efficiency",
+    ]
+    social = ["Social and employee matters"]
+    governance = ["Social"]
+
+    number_financial_products = len(esg_data["ISIN"].unique())
+
+    # Define metric of ESG for df_pure
+    environment_ranking = np.zeros(shape=(number_financial_products, 1))
+    social_ranking = np.zeros(shape=(number_financial_products, 1))
+    governance_ranking = np.zeros(shape=(number_financial_products, 1))
+
+    # Retrieve the metric values for each ISIN
+
+    for i, isin in enumerate(df_pure["ISIN"]):
+        # Create the dataframe with only the stock isin
+        df_isin = df[df["ISIN"] == isin]
+
+        for j, classif in enumerate(classification):
+            # Create a subcategory desired
+            df_isin_class = df_isin[df_isin["ESGClassification"] == classif]
+            # Get the pourcentage for this category
+            pourcentage = (
+                df_isin_class["ESGClassSymbol"].str.lower().str.contains("yes").mean()
+            )
+            if classif in environment:
+                environment_ranking[i] += pourcentage
+            elif classif in social:
+                social_ranking[i] = pourcentage
+            elif classif in governance:
+                governance_ranking[i] = pourcentage
+
+    # Mean for the number of factors
+    environment_ranking = environment_ranking / len(environment)
+    social_ranking = social_ranking / len(social)
+    governance_ranking = governance_ranking / len(governance)
+
+    # Make the assumption that if it is nan it is 0
+
+    environment_ranking_no_nan = np.nan_to_num(environment_ranking)
+    social_ranking_no_nan = np.nan_to_num(social_ranking)
+    governance_ranking_no_nan = np.nan_to_num(governance_ranking)
+
+    # Put the new data
+    df_pure["Environment"] = environment_ranking_no_nan
+    df_pure["Social"] = social_ranking_no_nan
+    df_pure["Governance"] = governance_ranking_no_nan
+
+    return df_pure
--- src/financial_data_api.py
+++ src/financial_data_api.py
@@ -6,6 +6,8 @@ import urllib.request
 import ssl
 import json
 from typing import List, Dict, Any
+import pandas as pd
+import numpy as np
 
 
 class APIError(Exception):
@@ -55,11 +57,22 @@ class FinancialDataAPI:
         """
         Retrieve instrument basic attributes using scheme and ids.
         """
-        end_point = "/v1/instruments/referenceData/instrumentBase"
+        end_point = "/v1/listings/referenceData/instrumentBase"  # "/v1/instruments/referenceData/instrumentBase"
+        return self._http_request_with_scheme_id(end_point, scheme, instruments)
+
+    def instrumentMarkets(self, scheme: str, instruments: List[str]) -> Dict[str, Any]:
+        """
+        Retrive markets in which instruments are listed
+        """
+        end_point = "/v1/instruments/referenceData/instrumentMarkets"
         return self._http_request_with_scheme_id(end_point, scheme, instruments)
 
     def endOfDayHistory(
-        self, scheme: str, listings: List[str], dateFrom: str, dateTo: str = ""
+        self,
+        scheme: str,
+        listings: List[str],
+        dateFrom: str,
+        dateTo: str = "",
     ) -> Dict[str, Any]:
         """
         Retrieve End of Day Timeseries data.
@@ -72,6 +85,209 @@ class FinancialDataAPI:
             "dateTo": dateTo,
         }
         return self._http_request(end_point, query_string)
+
+
+def get_data(
+    financial_object: FinancialDataAPI,
+    instruments: list[str],
+    start_date: str,
+    end_date: str,
+):
+    """
+    inputs:
+        financial_object: instance of FinancialDataAPI
+        instruments: list of intruments (with BC suffix)
+        start_date/end_date: range time
+    output:
+        data: dictionary {lookup, lookupStatus, marketData}
+    """
+
+    # get data
+    data = financial_object.endOfDayHistory(
+        "ISIN_BC", instruments, start_date, end_date
+    )
+    data = json.dumps(data, sort_keys=True, indent=4)
+    data = json.loads(data)["data"]["listings"]
+
+    return data
+
+
+def get_markets_correct_currency(
+    financial_object: FinancialDataAPI,
+    scheme: str,
+    instruments: list[str],
+    accessible_markets: list[int],
+    currency: str = "CHF",
+):
+    """get the markets bc in which the fund is in the given currency"""
+    # get instrument with bc on all markets
+    instruments_bc_all_markets = [
+        [instruments[i] + f"_{bc}" for i, bc in enumerate(accessible_markets[j])]
+        for j in range(len(accessible_markets))
+    ]
+
+    # flatten
+    instruments_bc_all_markets = [
+        item for sublist in instruments_bc_all_markets for item in sublist
+    ]
+
+    # get data
+    data = financial_object.instrumentBase(
+        scheme,
+        [markets for markets in instruments_bc_all_markets],
+    )
+
+    def get_currency(data):
+        try:
+            return data["lookup"]["listingCurrency"] == currency
+        except:
+            return False
+
+    # keep currency given
+    to_keep = list(
+        map(
+            lambda dict_: get_currency(dict_),
+            data["data"]["listings"],
+        )
+    )
+    data = list(
+        set(
+            [
+                market
+                for i, market in enumerate(instruments_bc_all_markets)
+                if to_keep[i]
+            ]
+        )
+    )
+
+    # keep only one from market
+    data = list(
+        map(
+            lambda l: "_".join(l),
+            pd.DataFrame(list(map(lambda s: s.split("_"), data)))
+            .drop_duplicates(subset=[0])
+            .to_numpy(),
+        )
+    )
+
+    return data
+
+
+def get_accessible_markets_bc(
+    financial_object: FinancialDataAPI,
+    scheme: str,
+    instruments: list[str],
+    accessible_markets: pd.DataFrame,
+) -> list[str]:
+    """
+    get usable bc of instruments
+    """
+
+    def get_bc(dict_):
+        return list(map(lambda val: val["bc"], dict_))
+
+    data = financial_object.instrumentMarkets(scheme, instruments)
+    all_markets_bc = list(
+        map(
+            lambda inst_dict: get_bc(inst_dict["referenceData"]["instrumentMarkets"]),
+            data["data"]["instruments"],
+        )
+    )
+
+    def check_accessible(markets: list[int], accessible_markets: list[int]):
+        return [bc for bc in markets if bc in accessible_markets]
+
+    return list(map(lambda x: check_accessible(x, accessible_markets), all_markets_bc))
+
+
+def get_financial_ts(data):
+    """return only financial time series
+
+    input:
+        data: dictionary
+    """
+
+    def get_price(dict_):
+        try:
+            return list(
+                map(lambda l: l["close"], dict_["marketData"]["endOfDayHistory"])
+            )
+        except:
+            return [-1]
+
+    prices = list(map(lambda dict_: get_price(dict_), data))
+    prices = np.array([item for sublist in prices for item in sublist])
+
+    temp = list(
+        map(
+            lambda l: (
+                np.full(len(l["marketData"]["endOfDayHistory"]), get_instrument_name(l))
+                if len(l["marketData"]["endOfDayHistory"]) != 0
+                else [1]
+            ),
+            data,
+        )
+    )
+
+    def get_date(dict_):
+        try:
+            # print(dict_)
+            return list(map(lambda l: l["sessionDate"], dict_))
+        except:
+            return [-1]
+
+    dates = list(map(lambda l: get_date(l["marketData"]["endOfDayHistory"]), data))
+    dates = np.array([item for sublist in dates for item in sublist])
+
+    temp = np.array([item for sublist in temp for item in sublist])
+
+    # inst = np.array(
+    #     list(
+    #         map(
+    #             lambda dict_: (
+    #                 np.full(
+    #                     len(dict_["marketData"]["endOfDayHistory"]),
+    #                     dict_["requestedId"],
+    #                 )
+    #                 if len(dict_["marketData"]["endOfDayHistory"]) != 0
+    #                 else [1]
+    #             ),
+    #             data,
+    #         )
+    #     )
+    # )
+    return dates, prices, temp
+
+    # instrument_isin = data["requestedId"].split("_")[0]
+    # instrument_name = f"{get_instrument_name(data)} ({instrument_isin})"
+    # data_plot = pd.DataFrame(
+    #     columns=["date", "close_price", "instrument_isin", "instrument_name"]
+    # )
+    # for i, dict_ in enumerate(data["marketData"]["endOfDayHistory"]):
+    #     try:
+    #         tmp = pd.DataFrame(
+    #             [
+    #                 [
+    #                     dict_["sessionDate"],
+    #                     dict_["close"],
+    #                     instrument_isin,
+    #                     instrument_name,
+    #                 ]
+    #             ],
+    #             columns=data_plot.columns,
+    #         )
+    #         data_plot = pd.concat([data_plot, tmp], axis=0)
+    #     except:
+    #         pass
+    # return data_plot.reset_index(drop=True)
+
+
+def get_instrument_name(data_instrument: Dict) -> str:
+    """returns the name of the instrument given"""
+    return data_instrument["lookup"]["listingShortName"]
+
+def get_instrument_name(data_instrument: Dict):
+    return data_instrument["lookup"]["listingShortName"]
 
 
 if __name__ == "__main__":
